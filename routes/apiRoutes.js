57f9cd3d39aec556cf53682980262c24d7f91a99
Carina Reyes <cereyes1792@gmail.com>
2020-08-05 23:21:04 -0500

    Added api routes file with get and post routes for api/workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,26 @@
-const db = require("../models");
+const workout = require("../models/index.js");
 
-module.exports = () => {};
+module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    workout
+      .find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+        res.json(err);
+      });
+  });
+  app.post("/api/workouts", (req, res) => {
+    workout
+      .create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+        res.json(err);
+      });
+  });
+};
